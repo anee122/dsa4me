848150170e1040db6249b1f34d1117132bb2b7ad
ane122 <chienvu20702@gmail.com>
2026-08-22 10:15:18 +0700

    vector dsaptit 22.08.2026

---
+++ stl/vector.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    //khai bao vector rong
+    vector<int> v1;
+
+    //khai bao vector co phan tu
+    vector<int> v2 = {1,2,3,4,5};
+
+    //khai bao vector co san n phan tu
+    int n = 20;
+    vector<int> v3(n);
+
+    //khai bao vector co n phan tu cung gia tri val
+    int val = 0;
+    vector<int> v4(n,val);
+
+    return 0;
+}
